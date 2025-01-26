981425603478c97f17ca8024b2295c9cb9dc7c02
Dallas Caley <dallascaley@gmail.com>
2025-01-26 08:45:38 -0800

    First commit

---
+++ vue/Breakroom/src/components/Signup.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+    <span>Sign Up</span>
+    <div>
+      <label>Username</label>
+      <input type="text" required>
+    </div>
+    <div>
+      <label>Password</label>
+      <input type="text" required>
+    </div>
+    <button type="submit">Create User</button>
+  </form>
+</template>
---
+++ vue/Breakroom/src/components/TheWelcome.vue
@@ -0,0 +1,9 @@
+<script setup>
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+
+  <div>This is Breakroom</div>
+
+</template>
