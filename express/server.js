72eeb1c23ff502ba7fd37393eac69a9ea10fdcc9
Dallas Caley <dallascaley@gmail.com>
2025-02-02 09:32:52 -0800

    More stuff, getting postgres setup etc...

--- express/server.js
+++ express/server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   console.log('whatever')
   //res.send('Hi') // just sends Hi
   //res.sendStatus(500) // just sends a 500 error
   //res.status(500).send('Hi') // does both things
-  res.status(500).json({message: "you done fucked up!"})  //this
+  //res.status(500).json({message: "you done fucked up!"})  //this
   //res.download('server.js')
+  res.render("index", { text2: 'World duh...'})
 })
 
 app.listen(3001)
