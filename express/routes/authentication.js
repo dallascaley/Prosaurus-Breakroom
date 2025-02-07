eb06e58cb0e93306b819e4e33e754423ee242f4f
Dallas Caley <dallascaley@gmail.com>
2025-02-07 10:54:57 -0800

    Email working!!!

--- express/routes/authentication.js
+++ express/routes/authentication.js
@@ -1,17 +1,37 @@
 const express = require('express');
+const sgMail = require('@sendgrid/mail');
 const router = express.Router();
 const { getClient } = require('../utilities/db');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // Define authentication-related routes
 router.post('/signup', async (req, res) => {
   console.log('Request Body');
   console.log(req.body);
 
-  const client = await getClient();
+  console.log('API KEY');
+  console.log(process.env.SENDGRID_API_KEY);
 
-  const result = await client.query('INSERT INTO "user" (name, password) VALUES ($1, $2);', [req.body.name, req.body.password]);
+  //const client = await getClient();
 
-  res.json(result.rows);
+  //const result = await client.query('INSERT INTO "user" (name, password) VALUES ($1, $2);', [req.body.name, req.body.password]);
+
+  const msg = {
+    to: 'dallascaley@gmail.com',
+    from: 'admin@prosaurus.com',
+    subject: 'This is the subject',
+    text: 'Here is your email',
+  };
+
+  sgMail.send(msg)
+    .then(() => {
+      res.status(200).send('Email sent successfully!');
+    }).catch((error) => {
+      res.status(500).send('Error sending email: ' + error.message);
+    });
+
+  //res.json(result.rows);
   //res.send('ah yea whatever, signup i guess');
 });
 
