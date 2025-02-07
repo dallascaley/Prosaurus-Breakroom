3af8127389e599c90b2b4ead52c3a1904a083f77
Dallas Caley <dallascaley@gmail.com>
2025-02-06 16:02:51 -0800

    Researching email methods

--- express/server.js
+++ express/server.js
@@ -15,40 +15,11 @@ app.use(express.json());
 app.use('/auth', authentication);
 
 
-// db.js
-const { Pool } = require('pg');
 
-// Create a new pool of connections
-const pool = new Pool({
-  user: 'splort',         // The PostgreSQL user
-  host: 'localhost',         // PostgreSQL server address (use your VM's IP if needed)
-  database: 'breakroom',     // Name of your database
-  password: 'glorp', // Password for the user
-  port: 5432,                // Default PostgreSQL port
-});
-
-// Export the pool for use in other files
-module.exports = pool;
-
-// Default route (which includes a bunch of shit from other demos)
+// Default route
 app.get('/', async (req, res) => {
-  console.log('whatever')
-  //res.send('Hi') // just sends Hi
-  //res.sendStatus(500) // just sends a 500 error
-  //res.status(500).send('Hi') // does both things
-  //res.status(500).json({message: "you done fucked up!"})  //this
-  //res.download('server.js')
-
-  try {
-    // Query to get all users (assuming you have a "user" table)
-    const result = await pool.query('SELECT * FROM "user"');
-    console.log('duh');
-    res.json(result.rows); // Send the results back to the client
-  } catch (err) {
-    console.error('Error executing query', err.stack);
-    res.status(500).send('Internal Server Error');
-  }
-  //res.render("index", { text2: 'World duh...'});
+  console.log('backend main page called for some damn reason')
+  res.send('Hi, this is the backend, call an actual endpoint or something')
 })
 
 app.listen(3000)
