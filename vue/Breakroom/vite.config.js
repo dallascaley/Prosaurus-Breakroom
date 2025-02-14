1f7a95f05412da6ecf7a5f481522baf764041e6c
Dallas Caley <dallascaley@gmail.com>
2025-02-13 16:53:31 -0800

    Reverse proxy working for vite

--- vue/Breakroom/vite.config.js
+++ vue/Breakroom/vite.config.js
@@ -19,25 +19,10 @@ export default defineConfig({
     },
   },
   server: {
-    https: {
-      key: fs.readFileSync('/home/dallas/prosaurus/key.pem'),
-      cert: fs.readFileSync('/home/dallas/prosaurus/cert.pem'),
-    },
     host: '0.0.0.0', // This is important for network access
     port: 5173, // You can adjust the port if needed
-    //open: true,
-    //middlewareMode: true,
     watch: {
       usePolling: true,
     },
-    /*
-    proxy: {
-      '/': {
-        target: 'https://10.6.0.48:5173',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-    */
   },
 });
